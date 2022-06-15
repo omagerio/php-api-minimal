3c6b99b40ee47a3da691db4332571272f5202e87
omar <omar>
2022-06-15 12:06:01 +0200

    fix

--- api/inc/includes.php
+++ api/inc/includes.php
@@ -31,8 +31,8 @@ function handleRequest($request)
     foreach ($handlersFiles as $handlerFile) {
         if (substr($handlerFile, -4) == ".php") {
             $include_response = require(__DIR__ . "/../handlers/" . $handlerFile);
-            if($include_response != null){
-                break;
+            if ($include_response != null) {
+                return $response;
             }
         }
     }
@@ -48,38 +48,3 @@ $orm = new DbOrm(
     $configs->db_pass,
     $configs->db_name
 );
-
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: *");
-header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Credentials: *");
-header("Content-type: application/json; charset=utf8");
-
-$response = new ApiResponse();
-
-try {
-
-    if ($_SERVER["REQUEST_METHOD"] == "GET") {
-        $input = $_GET["payload"];
-    } else {
-        $input = file_get_contents("php://input");
-    }
-
-    $request = json_decode($input);
-    $jsonError = json_last_error();
-    if ($jsonError) {
-        throw new Exception("Json Error: " . $jsonError);
-    }
-
-    if($request == null){
-        $request = new stdClass();
-    }
-
-    $request->headers = getallheaders();
-    $response = handleRequest($request);
-} catch (Exception $e) {
-    $response->errors[] = $e->getMessage();
-}
-
-echo json_encode($response);
-die();
---
+++ api/inc/init.php
@@ -0,0 +1,36 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Credentials: *");
+header("Content-type: application/json; charset=utf8");
+
+$response = new ApiResponse();
+
+try {
+
+    if ($_SERVER["REQUEST_METHOD"] == "GET") {
+        $input = $_GET["payload"];
+    } else {
+        $input = file_get_contents("php://input");
+    }
+
+    $request = json_decode($input);
+    $jsonError = json_last_error();
+    if ($jsonError) {
+        throw new Exception("Json Error: " . $jsonError);
+    }
+
+    if($request == null){
+        $request = new stdClass();
+    }
+
+    $request->headers = getallheaders();
+    $response = handleRequest($request);
+} catch (Exception $e) {
+    $response->errors[] = $e->getMessage();
+}
+
+echo json_encode($response);
+die();
